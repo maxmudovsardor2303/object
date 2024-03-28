cfd37e904b264b74682f25c1d96d28a796875f78
Your Name <you@example.com>
2024-03-28 08:45:10 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,86 @@
+// 1-masala
+// let obj = {name: "Alisher", age: 12, addres: "Tashkent"}
+// const saveobjvalues = (params)=>{
+//     let values = []
+//     for (let key in params ){
+//         values.push(params[key])
+//     }
+//     console.log(values);
+// }
+// saveobjvalues(obj)
+// const saveobjkeys = (params)=>{
+//     let keys = []
+//     for (let key in params ){
+//         keys.push(key)
+//     }
+//     console.log(keys);
+// }
+// saveobjkeys(obj)
+
+
+
+
+
+
+
+// 2-masala
+// function arrays(...params) {
+//     let item = [2, 4, 6, 7, 8, 10, 12,]
+//      for (let i = 0; i < item.length; i++) {
+//         if (item[i] == params) {
+//             console.log("Ichida bor")
+//         }
+//         else{
+//             console.log("Ichida yuq");
+//         }
+//     }
+// }
+// arrays(4)
+
+
+
+
+// 3-masala
+// function arrays(...params) {
+//     let box = 0
+//     let item = ["olma", "davlat", "xalq", "dasturchi", "dasturlash", "google", "boolean'"]
+//     for (let i = 0; i < item.length; i++) {
+//         if (massiv[i] == params) {
+//             box ++
+//             console.log("Ichida mavjud");
+//         }
+//     }
+//     if (box == 0) {
+//         console.log("Ichida mavjud emas");
+//     }
+// }
+// arrays("dasturchi")
+
+
+
+
+// 4-task
+// function arrays(...params) {
+//     let desc = 0
+//     let massiv = ["olma", "davlat", "xalq", "dasturchi", "dasturlash", "google", "boolean"]
+//     for (let i = 0; i < massiv.length; i++) {
+//         if (massiv[i] == params) {
+//             desc++
+//             console.log(i);
+//         }
+//     }
+//     if (desc == 0) {
+//         console.log("Ichida mavjud emas");
+//     }
+// }
+// arrays("dasturlash")
+
+
+// 5-masala
+// let a = {a_1: 14, a_2: 89, a_3: 43}
+// let b = {b_1: 5, b_2: 34, b_3: 23}
+// let c = {c_1: 32, b_2: 76, c_3: 65}
+// let t = Object.assign(a, b, c)
+// console.log(t);
+
+
